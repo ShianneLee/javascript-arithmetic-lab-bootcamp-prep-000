88214c8e30ff582bc2f2124265e51fb81a122cc9
Shianne Lee <shiannelee@live.ca>
2017-11-07 21:53:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 function add(){
-  
+
 }
 function subtract(){
-  
+
 }
 function multiply(){
-  
+
 }
 function divide(){
-  
+
 }
 function inc(n){
-  
+
 }
 function dec(n){
-  
+
 }
 function makeInt(n){
-  
+
 }
 function preserveDecimal(n){
-  
+
 }
